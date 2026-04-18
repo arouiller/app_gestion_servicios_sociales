76a74898881030d53f974a8a3e9a6b973daac91a
arouiller <alejandro.rouiller@gmail.com>
2026-04-17 21:40:00 -0300

    feat(models): agregar campo abreviacion a TipoDeGrupo

--- backend/src/models/TipoDeGrupo.js
+++ backend/src/models/TipoDeGrupo.js
@@ -4,6 +4,16 @@ const sequelize = require('../config/database');
 const TipoDeGrupo = sequelize.define('TipoDeGrupo', {
   tipo_de_grupo_numero: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: false },
   tipo_de_grupo_nombre: { type: DataTypes.STRING(100), allowNull: false },
+  abreviacion: {
+    type: DataTypes.STRING(10),
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: {
+        msg: 'La abreviación no puede estar vacía',
+      },
+    },
+  },
   fecha_creacion: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
   fecha_actualizacion: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
 }, { tableName: 'tipos_de_grupo', timestamps: false });
