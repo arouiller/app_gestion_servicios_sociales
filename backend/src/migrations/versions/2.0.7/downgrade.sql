fda300598d5c4c47160022a487323560f55df8db
arouiller <alejandro.rouiller@gmail.com>
2026-04-17 22:19:45 -0300

    fix(migrations): hacer adición de campos condicional a su existencia (IF NOT EXISTS)

--- backend/src/migrations/versions/2.0.7/downgrade.sql
+++ backend/src/migrations/versions/2.0.7/downgrade.sql
@@ -1,2 +1,2 @@
-ALTER TABLE tipos_de_grupo DROP COLUMN abreviacion;
-ALTER TABLE tipos_de_plan DROP COLUMN abreviacion;
+ALTER TABLE tipos_de_grupo DROP COLUMN IF EXISTS abreviacion;
+ALTER TABLE tipos_de_plan DROP COLUMN IF EXISTS abreviacion;
--- backend/src/migrations/versions/2.0.7/upgrade.sql
+++ backend/src/migrations/versions/2.0.7/upgrade.sql
@@ -1,5 +1,5 @@
--- Agregar campo abreviacion a tipos_de_grupo
-ALTER TABLE tipos_de_grupo ADD COLUMN abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER tipo_de_grupo_nombre;
+-- Agregar campo abreviacion a tipos_de_grupo (condicional a su existencia)
+ALTER TABLE tipos_de_grupo ADD COLUMN IF NOT EXISTS abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER tipo_de_grupo_nombre;
 
--- Agregar campo abreviacion a tipos_de_plan
-ALTER TABLE tipos_de_plan ADD COLUMN abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER tipo_plan_nombre;
+-- Agregar campo abreviacion a tipos_de_plan (condicional a su existencia)
+ALTER TABLE tipos_de_plan ADD COLUMN IF NOT EXISTS abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER tipo_plan_nombre;
