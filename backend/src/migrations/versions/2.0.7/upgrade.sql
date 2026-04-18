aa76beec604ea77b53faa612a4f3d3ca16f0f168
arouiller <alejandro.rouiller@gmail.com>
2026-04-17 21:39:15 -0300

    fix(migrations): simplificar migración 2.0.7 eliminando MODIFY redundante

--- backend/src/migrations/versions/2.0.7/upgrade.sql
+++ backend/src/migrations/versions/2.0.7/upgrade.sql
@@ -1,17 +1,9 @@
 -- Agregar campo abreviacion a tipo_grupo
 ALTER TABLE tipo_grupo
-ADD COLUMN abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER nombre;
-
-ALTER TABLE tipo_grupo
-MODIFY COLUMN abreviacion VARCHAR(10) NOT NULL;
-
-CREATE UNIQUE INDEX idx_tipo_grupo_abreviacion ON tipo_grupo(abreviacion);
+ADD COLUMN abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER nombre,
+ADD UNIQUE INDEX idx_tipo_grupo_abreviacion (abreviacion);
 
 -- Agregar campo abreviacion a tipo_plan
 ALTER TABLE tipo_plan
-ADD COLUMN abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER nombre;
-
-ALTER TABLE tipo_plan
-MODIFY COLUMN abreviacion VARCHAR(10) NOT NULL;
-
-CREATE UNIQUE INDEX idx_tipo_plan_abreviacion ON tipo_plan(abreviacion);
+ADD COLUMN abreviacion VARCHAR(10) NOT NULL DEFAULT '' AFTER nombre,
+ADD UNIQUE INDEX idx_tipo_plan_abreviacion (abreviacion);
