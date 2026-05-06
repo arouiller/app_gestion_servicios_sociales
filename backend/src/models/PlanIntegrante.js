5c6788b0590b399c850d5b7453cd3118d6b409a8
arouiller <alejandro.rouiller@gmail.com>
2026-05-06 14:36:21 -0300

    feat(models): agregar FK zona_id a PlanIntegrante

--- backend/src/models/PlanIntegrante.js
+++ backend/src/models/PlanIntegrante.js
@@ -8,6 +8,21 @@ const PlanIntegrante = sequelize.define('PlanIntegrante', {
   rol: { type: DataTypes.ENUM('titular','integrante'), allowNull: false },
   credencial: { type: DataTypes.CHAR(1), allowNull: false },
   fecha_creacion: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+  zona_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'zonas',
+      key: 'id'
+    }
+  },
 }, { tableName: 'plan_integrantes', timestamps: false });
+
+PlanIntegrante.associate = (models) => {
+  PlanIntegrante.belongsTo(models.Zona, {
+    foreignKey: 'zona_id',
+    as: 'zona'
+  });
+};
 
 module.exports = PlanIntegrante;
